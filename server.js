663548af8050b31c29f1d9d0cec8a3f3fb3f9a5d
Younis <younis@Youniss-MacBook-Pro.local>
2021-05-19 17:52:20 -0400

    Node res with diffrent url

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
 
-  // what we want to respond back
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head> <title> My first Page </title> </head>");
-  res.write("<body><h1>Hello from node</h1></body>")
-  res.write("</head>");
-  res.end();
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head> <title> My first Page </title> </head>");
+    res.write("<body><h1>Home page </h1></body>");
+    res.write("</head>");
+    return res.end();
+  } else if (url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head> <title> My first Page </title> </head>");
+    res.write("<body><h1>About</h1></body>");
+    res.write("</head>");
+    return res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head> <title> My first Page </title> </head>");
+    res.write("<body><h1>no page here</h1></body>");
+    res.write("</head>");
+    return res.end();
+  }
 });
 
 server.listen(1111, () => {
