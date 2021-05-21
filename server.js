5876c995a054406017b45f0117ef7c1e872cb71f
Younous <Younissadam2020@gmail.com>
2021-05-21 17:20:33 -0400

    Parsing Request Body, using req.on as an ecent listener

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-
+  const method = req.method;
   if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
-    res.write("<head> <title> My first Page </title> </head>");
-    res.write("<body><h1>Home page </h1></body>");
-    res.write("</head>");
-    return res.end();
-  } else if (url === "/about") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head> <title> My first Page </title> </head>");
-    res.write("<body><h1>About</h1></body>");
-    res.write("</head>");
-    return res.end();
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head> <title> My first Page </title> </head>");
-    res.write("<body><h1>no page here</h1></body>");
-    res.write("</head>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
     return res.end();
   }
+  if (url === "/message" && method === "POST") {
+    // For an event listener, ON allows us to listener to data as a request,
+    // and we want to push it to an empty array to have access to it later
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    // End event is gonna be fired after the first one is done.
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(1111, () => {
-  console.log("server is running");
-});
+server.listen(3000);
