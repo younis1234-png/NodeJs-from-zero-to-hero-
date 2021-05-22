d1c4e5906a36aa8dd1507e50108b042f3109587c
Younous <Younissadam2020@gmail.com>
2021-05-22 16:36:01 -0400

    filter for get request or post request insted of .use because thats gonna fired everytime

--- server.js
+++ server.js
@@ -5,28 +5,24 @@ const app = express();
 
 const routes = require("./routes");
 
-// the parsing of the body: is for the incoming request to hav access to out=r rea.body
-// the urlencoded this a function, it request a middleware. At the end it will call next. but it will do res.body parser before
-// with that now WE GONNA GET THE BODY PARSER INSTED OF UNDEFINED ON LINE 23
-// THAT WILL GIVE US AN OBJE3CFT { title: 'INPUT' }
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded());
 
-app.use("/add-product", (req, res, next) => {
-  // console.log("In the second  middleware");
+//.get only gonna get fired when we have a get request
+app.get("/add-product", (req, res, next) => {
   res.send(
     "<form action ='/product' method='POST'><input type='text' name='title' /> <button type='submit'>Add product </button> </form>"
   );
 });
 
-app.use("/product", (req, res, ext) => {
+// .post is gonna get fired when we have a post request
+app.post("/product", (req, res, ext) => {
   // req body is
   console.log(req.body);
   res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  // console.log("In the second  middleware");
   res.send("<h1>Hello from express");
 });
 
