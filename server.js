489c79be44b2ff777af259e931115f58af074f80
Younous <Younissadam2020@gmail.com>
2021-05-22 16:32:14 -0400

    Parsing incoming requests with body-parser (if we dont do that when we log our input it will be undefined)

--- server.js
+++ server.js
@@ -5,17 +5,31 @@ const app = express();
 
 const routes = require("./routes");
 
-// use allow us the add another middleware function
-// next is a function that gonna be passed to the express function 
-// In this case the request will never reach the second middlewaare 
-// Next allows the request to continue to the next middleware in line
-app.use((req, res, next) => {
-  console.log("In the middleware");
-});
-app.use((req, res, next) => {
-  console.log("In the second  middleware");
+// the parsing of the body: is for the incoming request to hav access to out=r rea.body
+// the urlencoded this a function, it request a middleware. At the end it will call next. but it will do res.body parser before
+// with that now WE GONNA GET THE BODY PARSER INSTED OF UNDEFINED ON LINE 23
+// THAT WILL GIVE US AN OBJE3CFT { title: 'INPUT' }
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
+
+app.use("/add-product", (req, res, next) => {
+  // console.log("In the second  middleware");
+  res.send(
+    "<form action ='/product' method='POST'><input type='text' name='title' /> <button type='submit'>Add product </button> </form>"
+  );
 });
 
-const server = http.createServer(routes);
+app.use("/product", (req, res, ext) => {
+  // req body is
+  console.log(req.body);
+  res.redirect("/");
+});
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+  // console.log("In the second  middleware");
+  res.send("<h1>Hello from express");
+});
+
+app.listen(3000, () => {
+  console.log("server is running " + 3000);
+});
